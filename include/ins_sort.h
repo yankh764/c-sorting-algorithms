b88f4a3e72236c40f7e5089b05ef3dbb8fa6f099
yankh764 <yankh18@protonmail.com>
2021-09-24 19:43:10 +0300

    Working

--- include/ins_sort.h
+++ include/ins_sort.h
@@ -1,6 +1,7 @@
 #ifndef INS_SORT_H
 #define INS_SORT_H
 
-int *insertion_sort_int(int *, const unsigned int);
+void insertion_sort_int(int *, const unsigned int);
+void insertion_sort_float(float *, const unsigned int);
 
 #endif
--- src/ins_sort.c
+++ src/ins_sort.c
@@ -26,7 +26,7 @@ static void insertion_sort_int_backward(int array[], unsigned int key)
  * I decided to enter the arguments type src[] and dst[] like
  * this and not *src and *dst to avoid confusion for the users
  */
-int *insertion_sort_int(int array[], const unsigned int size)
+void insertion_sort_int(int array[], const unsigned int size)
 {
         unsigned int i, key;
 
@@ -38,5 +38,46 @@ int *insertion_sort_int(int array[], const unsigned int size)
                          * equal to i since array[key] was moved t array[i] 
                          */
                         insertion_sort_int_backward(array, i);
+                }
+}
+
+/*
+ * Swap array[key] and array[key-1] 
+ * (array[key] to array[key-1] and array[key-1] to array[key])
+ */
+static void insertion_sort_float_swap(float array[], const unsigned int key) 
+{
+        float tmp;
+       
+        tmp = array[key];
+        array[key] = array[key-1];
+        array[key-1] = tmp;
+}
+
+static void insertion_sort_float_backward(float array[], unsigned int key)
+{
+        long int i = (long int) key - 1;
+
+        for (; i>-1; i--, key--)
+                if (array[key] < array[i])
+                        insertion_sort_float_swap(array, key);
+}
+
+/*
+ * I decided to enter the arguments type src[] and dst[] like
+ * this and not *src and *dst to avoid confusion for the users
+ */
+void insertion_sort_float(float array[], const unsigned int size)
+{
+        unsigned int i, key;
+
+        for (i=0, key=1; key<size; i++, key++)
+                if (array[key] < array[i]) {
+                        insertion_sort_float_swap(array, key);
+                        /* 
+                         * After the previous swap the current key is 
+                         * equal to i since array[key] was moved t array[i] 
+                         */
+                        insertion_sort_float_backward(array, i);
                 }
 }
