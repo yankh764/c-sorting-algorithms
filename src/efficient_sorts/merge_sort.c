5fef0c27b609bda0519dcfab363eb9e4400ced99
yankh764 <yankh18@protonmail.com>
2021-10-05 19:45:24 +0300

    Working

--- src/efficient_sorts/merge_sort.c
+++ src/efficient_sorts/merge_sort.c
@@ -12,39 +12,34 @@
 
 /*
  * Copy a partition from the main array "from" to a subarray "to"
- * from the index from[begin] to the index from[end]. Assume "to"
- * is big enough.
+ * from the index from[begin] to the index from[end]. Start copying to "to[i]".
+ * Assume "to" is big enough.
  */
-static void copy_partition(const int from[], int to[],
-                           const unsigned int begin, 
-                           const unsigned int end)
+static void copy_partition_int(const int from[], unsigned int begin, 
+                               unsigned int end, int to[], unsigned int i)
 {
-        unsigned int i;
-        
-        for (i=begin; i<=end; i++)
-                to[i] = from[i];
+        for (; begin<=end; begin++, i++)
+                to[i] = from[begin];
 }
 
 static void merge_int(const int left[], const unsigned int l_len, 
                       const int right[], const unsigned int r_len, 
                       int array[])
 {
-        unsigned int i_l, i_r, i;
+        unsigned int il, ir, ia;
 
-        for (i_l=i_r=i=0; ; i++) {
-                if (i_l<l_len && i_r<r_len) {
-                        if (left[i_l] < right[i_r])
-                                array[i] = left[i_l++];
-                        else
-                                array[i] = right[i_r++];
-                } else {
-                        if (i_l == l_len)
-                                copy_partition(right, array, i_r, r_len-1);
-                        else
-                                copy_partition(left, array, i_l, l_len-1);
-                        break;
-                }
+        for (il=0, ir=0, ia=0; (il<l_len && ir<r_len) ; ia++) {
+                if (left[il] < right[ir])
+                        array[ia] = left[il++];
+                else
+                        array[ia] = right[ir++];
         }
+        /* left[] is empty */
+        if (il == l_len)
+                copy_partition_int(right, ir, r_len-1, array, ia);
+        /* right[] is empty */
+        else
+                copy_partition_int(left, il, l_len-1, array, ia);
 }
 
 void merge_sort_int(int array[], const unsigned int len)
@@ -56,19 +51,11 @@ void merge_sort_int(int array[], const unsigned int len)
         int left[l_len]; 
 
         if (middle) {
-                copy_partition(array, left, 0, middle-1);
-                copy_partition(array, right, middle, len-1);
+                copy_partition_int(array, 0, middle-1, left, 0);
+                copy_partition_int(array, middle, len-1, right, 0);
                 merge_sort_int(left, l_len);
                 merge_sort_int(right, r_len);
                 merge_int(left, l_len, right, r_len, array);
         }
 }
 
-/*
-                        1 4 3 2
-                   1 4           3 2
-                 1     4       3     2
-                   1 4           2 3
-                        1 2 3 4        
-
-*/
